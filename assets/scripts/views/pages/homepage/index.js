dd6a93395c7056611481f3ebe5e35bb07728d642
Chris McClellan <chris@chrismcclellan.com>
2016-03-20 19:42:55 -0400

    alternating bg

--- assets/scripts/views/pages/homepage/index.js
+++ assets/scripts/views/pages/homepage/index.js
@@ -18,79 +18,99 @@ module.exports = View.extend({
     },
 
     onAttach: function() {
-        this.$list = this.$('.company-list');
+        this.$list = this.$('.company-list .company');
         this.$miniRanks = this.$('.mini.ranks .inner');
         this.$miniNames = this.$('.mini.names .inner');
+        this.$bgWrapper = this.$('.background-wrapper');
 
         this._onResize();
         // console.log('pages/homepage.onAttach');
     },
 
     _onResize: function(event) {
-        this._docHeight = $document.outerHeight() - $window.height();
-        this._miniRanksHeight = this.$miniRanks.outerHeight() - $window.height();
-        this._miniNamesHeight = this.$miniNames.outerHeight() - $window.height();
+        this._doc_height = $document.outerHeight() - $window.height();
+        this._mini_ranks_height = this.$miniRanks.outerHeight() - $window.height();
+        this._mini_names_height = this.$miniNames.outerHeight() - $window.height();
     },
 
     _onScroll: function(event) {
 
         // if (this.scrollLock) return console.log('scrollLocked - return');
 
-        this._docScrollTop = $document.scrollTop();
-        this._percentScrolled = this._docScrollTop / this._docHeight * 100;
+        var self = this;
 
-        var innerHeight;
-        var pixels
+        // Set scroll contextual information
+        this._scroll_top = $document.scrollTop();
+        this._percent_scrolled = this._scroll_top / this._doc_height * 100;
+        this._scroll_direction = this._scroll_top > this._last_scroll_top ? 'down' : 'up';
 
-        // scrollRanks
-        innerHeight = this._miniRanksHeight;
-        pixels = innerHeight * this._percentScrolled / 100;
+        // SCROLL LEFT COLUMNS
+        this._scrollRanks();
+        this._scrollNames();
+
+        this.$list.each(function(index) {
+
+            var $this = $(this);
+            var offset = $this.offset().top;
+            var height = $this.outerHeight();
+            var center = Math.round(offset + (height/2));
+            var win_height = $window.height();
+            var win_center = Math.round(self._scroll_top + (win_height/2));
+            var threshold = offset + height;
+            var is_current = false;
+            var scale = 1;
+
+            // find prev, current, and next
+            if (win_center > offset && win_center < threshold && !$this.hasClass('current')) {
+
+                self.$list.removeClass('previous').removeClass('current');
+                $this.prev().addClass('previous');
+                $this.addClass('current');
+
+                self._setCurrentBg($this.attr('data-blurry-bg'));
+
+            }
+
+        });
+    },
+
+    _setCurrentBg: function(bg_path) {
+
+        console.log(bg_path);
+
+        if (!bg_path) return;
+
+        var $backgrounds = this.$bgWrapper.find('.background');
+        if ($backgrounds.length > 2) {
+            $backgrounds.first().remove();
+        }
+
+        var $div = $('<div class="background" />').css({
+            'background-image': 'url("'+ bg_path + '")'
+        });
+
+        this.$bgWrapper.append($div);
+
+        window.setTimeout(function() {
+            $div.addClass('fade-in');
+        }, 30);
+
+    },
+
+    _scrollRanks: function() {
+        var inner_height = this._mini_ranks_height;
+        var pixels = inner_height * this._percent_scrolled / 100;
         this.$miniRanks.parent().scrollTop(pixels);
+    },
 
-
-        // crollNames
-        innerHeight = this._miniNamesHeight;
-        pixels = innerHeight * this._percentScrolled / 100;
+    _scrollNames: function() {
+        var inner_height = this._mini_names_height;
+        var pixels = inner_height * this._percent_scrolled / 100;
         this.$miniNames.parent().scrollTop(pixels);
     },
 
     _onScrollStop: function(event) {
 
-        if (this.scrollLock) return console.log('scrollLocked - return');
-        this.scrollLock = true;
-
-        // find nearest company and vertically center it in the viewport
-        var self = this;
-        var list = $('.company', this.$list);
-
-        var centered = _.find(list, function(item) {
-
-            var $item = $(item);
-            var scrollTop = $document.scrollTop() + ($window.height() / 2);
-            var offset = $item.offset().top;
-            var min = offset;
-            var max = offset + $item.outerHeight();
-
-            if (scrollTop > min && scrollTop < max) {
-                return true;
-            }
-        });
-
-        var $centered = $(centered);
-
-        // $('.company', this.$list)
-        //     .removeClass('current')
-        //     .removeClass('prev')
-        //     .removeClass('next');
-
-        // $centered.prev().addClass('prev');
-        // $centered.addClass('current');
-        // $centered.next().addClass('next');
-
-        // $('html, body').animate({ scrollTop: $centered.offset().top + ($window.height()/2)}, 100, function() {
-        //     // allow for scroll binding again
-        //     console.log('scroll callback');
-        //     self.scrollLock = false;
-        // });
+        console.log
     }
 });
